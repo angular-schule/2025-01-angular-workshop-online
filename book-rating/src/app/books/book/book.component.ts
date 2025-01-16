64fa667ab475f5657296234432eb016eb2859a48
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-16 11:02:16 +0100

    Events

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input, output } from '@angular/core';
 import { Book } from '../shared/book';
 import { CurrencyPipe } from '@angular/common';
 import { RatingComponent } from '../rating/rating.component';
@@ -13,4 +13,17 @@ export class BookComponent {
   // Input: hier fließen Daten von der Elternkomponente hinein
   // von oben nach unten
   book = input.required<Book>();
+
+  // Output: hier fließen Daten von hier zur Elternkomponente
+  // von unten nach oben
+  rateUp = output<Book>();
+  rateDown = output<Book>();
+
+  doRateUp() {
+    this.rateUp.emit(this.book());
+  }
+
+  doRateDown() {
+    this.rateDown.emit(this.book());
+  }
 }
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -29,15 +29,13 @@ export class DashboardComponent {
       }
     ]);
   }
+
+  doRateUp(book: Book) {
+    console.log('UP', book);
+  }
+
+  doRateDown(book: Book) {
+    console.log('DOWN', book);
+  }
 }
 
-
-/*
-TODO: Bücher anzeigen
-
-- Datenmodell
-- Daten
-- Anzeige
-  - Komponente für EIN Buch
-
-*/
