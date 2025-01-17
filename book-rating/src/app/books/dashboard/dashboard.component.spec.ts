d21adfb7b444587c457ab3c3fd228f629fc6a88a
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-17 10:56:17 +0100

    Test Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -11,7 +11,7 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     const ratingMock = {
       rateUp: (b: Book) => b,
-      rateDown: (b: Book) => b,
+      // rateDown: (b: Book) => b,
     };
 
     await TestBed.configureTestingModule({
@@ -23,11 +23,42 @@ describe('DashboardComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
+    // TS-Klasseninstanz
     component = fixture.componentInstance;
+
+    // Zugriff auf DOM-Element
+    // fixture.nativeElement.querySelector('p')
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call service.rateUp() for doRateUp()', () => {
+    // ARRANGE
+    // Service injecten, das ist in Wahrheit aber unser ratingMock
+    const rs = TestBed.inject(BookRatingService);
+
+    // Testbuch
+    const testBook = { isbn: '345534543' } as Book; // Type Assertion
+
+    // Methode präparieren
+    // spyOn(rs, 'rateUp').and.returnValue(testBook);
+    // spyOn(rs, 'rateUp').and.callFake(b => b)
+    // spyOn(rs, 'rateUp').and.callFake(() => testBook)
+    // Methode überwachen, aber originale Methode nicht wegwerfen,
+    // sondern weiterhin verwenden, um den Retun-Wert zu erzeugen
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    // ACT
+    // Methode in Komp aufrufen
+    component.doRateUp(testBook);
+
+    // ASSERT
+    // prüfen, ob Methode aufgerufen wurde
+    expect(rs.rateUp).toHaveBeenCalled();
+    expect(rs.rateUp).toHaveBeenCalledOnceWith(testBook);
   });
 });
