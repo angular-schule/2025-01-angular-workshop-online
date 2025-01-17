4a91f32f91df2dba46cd063a077fa62935b6eb70
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-17 15:45:36 +0100

    Detailseite

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
@@ -8,5 +10,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
+  #route = inject(ActivatedRoute);
+  #bs = inject(BookStoreService);
 
+  readonly book = signal<Book | undefined>(undefined);
+
+  constructor() {
+    // PULL
+    // const isbn = this.#route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+
+    // PUSH
+    this.#route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion
+      this.#bs.getSingle(isbn).subscribe(book => {
+        this.book.set(book);
+      });
+    });
+  }
 }
