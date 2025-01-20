a8ad4a478f601695fb638bee6e7d3e0f8808bd8b
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-20 12:38:19 +0100

    Reactive Forms

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,11 +1,58 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './book-create.component.html',
   styleUrl: './book-create.component.scss'
 })
 export class BookCreateComponent {
+  bookForm = new FormGroup({
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(7),
+        Validators.maxLength(15),
+        // Validators.pattern(/^[0-9]*$/)
+      ]
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(100)
+      ]
+    }),
+    description: new FormControl('', {
+      nonNullable: true,
+      validators: []
+    }),
+    rating: new FormControl(5, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+      ]
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(0)
+      ]
+    }),
+  });
 
+  isInvalid(control: FormControl): boolean {
+    return control.invalid && control.touched;
+  }
+
+  hasError(control: FormControl, errorCode: string): boolean {
+    // Hat dieses Control diesen bestimmten Fehler?
+    return control.hasError(errorCode) && control.touched;
+  }
 }
