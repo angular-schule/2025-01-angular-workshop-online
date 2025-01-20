bd7d4b5c999a2146cfc1cba5f12e8a8ec0b3c70c
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-20 11:04:34 +0100

    Vorbereitung Formular und Suche

--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { BookDetailsComponent } from "./book-details/book-details.component";
+import { BookSearchComponent } from "./book-search/book-search.component";
+import { BookCreateComponent } from "./book-create/book-create.component";
 
 export const booksRoutes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: ':isbn', component: BookDetailsComponent }
+  { path: 'search', component: BookSearchComponent },
+  { path: 'create', component: BookCreateComponent },
+  { path: ':isbn', component: BookDetailsComponent },
 ];
