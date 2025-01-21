27dea7172820f1e3185103bd0831c1a2423e9ed2
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-21 10:49:31 +0100

    RxJS: Observables und Operatoren

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Observer, Subscriber } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
 
 @Component({
@@ -23,7 +23,62 @@ export class CreatingComponent {
 
     /******************************/
 
-    
+
+    // of('Hamburg', 'Zürich', 'Wien', 'Leipzig', 'Köln')
+    // from([1,2,3,4,5,6])
+    // interval(1000)         // ---0---1---2---3---4---5---6 ...
+    // timer(3000)            // ---------0|
+    // timer(3000, 1000)      // ---------0---1---2---3---4---5---6 ...
+    // timer(0, 1000)         // 0---1---2---3---4---5---6 ...
+
+    timer(0, 1000).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    })
+
+
+    /******************************/
+
+    // Producer: generiert die Daten
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      const result2 = 5;
+
+      sub.next(result);
+      sub.next(result2);
+      sub.next(10);
+
+      setTimeout(() => sub.next(100), 1000)
+      setTimeout(() => sub.complete(), 3000)
+    }
+
+    // Observer: hört von außen zu,
+    // konsumiert die Daten
+    const obs: Observer<number> = {
+      next: (e: number) => console.log(e),
+      error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG')
+    };
+
+    // producer(obs);
+
+    // Observable: wrappt den Producer
+    // Schnittstelle zwischen Producer und Observer
+    const myObs$ = new Observable(producer)
+    // myObs$.subscribe(obs);
+
+    // $: Finnische Notation
+
+    /*myObs$.subscribe({ next: () => {} });
+    myObs$.subscribe({ error: () => {} });
+    myObs$.subscribe(() => {});*/
+
+
+
+
     /******************************/
   }
 
