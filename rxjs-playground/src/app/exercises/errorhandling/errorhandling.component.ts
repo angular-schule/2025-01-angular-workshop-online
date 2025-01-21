448b680a0c2e6ed2a864a8bc7825f81d29bbe7fb
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-21 14:02:41 +0100

    RxJS: Error Handling

--- rxjs-playground/src/app/exercises/errorhandling/errorhandling.component.ts
+++ rxjs-playground/src/app/exercises/errorhandling/errorhandling.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ReplaySubject, throwError, of, EMPTY, retry, catchError } from 'rxjs';
+import { ReplaySubject, throwError, of, EMPTY, retry, catchError, Observable } from 'rxjs';
 
 import { HistoryComponent } from '../../shared/history/history.component';
 import { DataService } from './data.service';
@@ -20,12 +20,21 @@ export class ErrorhandlingComponent {
 
   start() {
     this.#ds.getData().pipe(
+      catchError(err => {
+        // mit dem Fehler arbeiten
+        console.log('FEHLER:', err);
 
-      /******************************/
+        // Fehler ignorieren
+        // return EMPTY;
 
-      
-      /******************************/
+        // Fehler weiterwerfen
+        // return new Observable(sub => sub.error('FEHLER!!!!'));
+        // return throwError(() => 'BÖSER FEHLER!');
+        throw 'EXCEPTION!';
 
+        // Fehler ersetzen
+        // return of('Nichts', 'passiert!');
+      })
     ).subscribe({
       next: e => this.logStream$.next(e),
       error: err => this.logStream$.next('❌ ERROR: ' + err),
