cb9e600c66af4bbac3ec4a525031095352df4dd9
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-22 10:50:32 +0100

    aufräumen

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -3,12 +3,11 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 import { map, switchMap } from 'rxjs';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book-details',
-  imports: [RouterLink, AsyncPipe, JsonPipe],
+  imports: [RouterLink],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
@@ -20,12 +19,4 @@ export class BookDetailsComponent {
     map(params => params.get('isbn')!),
     switchMap(isbn => this.#bs.getSingle(isbn))
   ));
-
-  constructor() {
-    // PULL
-    // const isbn = this.#route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
-
-    // PUSH
-
-  }
 }
