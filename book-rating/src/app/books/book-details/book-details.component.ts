952e858653dcf436bc60903dfbd5e354c7b6c657
Ferdinand Malcher <ferdinand@malcher.media>
2025-01-22 10:41:44 +0100

    Detailseite mit AsyncPipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -3,10 +3,12 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 import { map, switchMap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
@@ -16,16 +18,16 @@ export class BookDetailsComponent {
 
   readonly book = signal<Book | undefined>(undefined);
 
+  book$ = this.#route.paramMap.pipe(
+    map(params => params.get('isbn')!),
+    switchMap(isbn => this.#bs.getSingle(isbn))
+  );
+
   constructor() {
     // PULL
     // const isbn = this.#route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
 
     // PUSH
-    this.#route.paramMap.pipe(
-      map(params => params.get('isbn')!),
-      switchMap(isbn => this.#bs.getSingle(isbn))
-    ).subscribe(book => {
-      this.book.set(book);
-    });
+
   }
 }
